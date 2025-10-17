09444050c9ca047546e850ce9fd364fbe5289bdc
jakubzajkowski <jakudjan123@gmail.com>
2025-10-17 11:21:30 +0200

    fix: settings fix

--- app/core/settings.py
+++ app/core/settings.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     gemini_api_key: str = "test_key"
     user_service_url: str = "http://localhost"
     user_cookie_name: str = "COWRITE_SESSION_ID"
-    database_url: str
+    database_url: str = "postgresql+asyncpg://user:password@localhost:5432/app_db"
 
     class Config:
         """Pydantic configuration."""
